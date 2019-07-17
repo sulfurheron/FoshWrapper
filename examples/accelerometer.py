31ee29d6318852d094a5ded28055ae50b1d29eb5
Kevin George <kevin@kindred.ai>
2019-07-17 13:31:02 -0700

    Converting bytearray to half precision floats

--- examples/accelerometer.py
+++ examples/accelerometer.py
@@ -4,15 +4,37 @@ import struct
 import sys
 import signal
 
+def littleEndianToInt8(data, offset):
+    x = littleEndianToUint8(data, offset)
+    if x & 0x80:
+        x = x - 256
+    return x
+
+def littleEndianToUint8(data, offset):
+    return data[offset]
+
+def littleEndianToInt16(data, offset):
+    return (littleEndianToInt8(data, offset + 1) << 8) + littleEndianToUint8(data, offset)
+
 ##subscribed functions
 def accel_callback(handle, data):
     #ok handle of commands will be store in "handle"
     #and accelerometer data will be store in "data" as bytearray
     #_, sensor_state, sensor_event, x, y, z = struct.unpack('!3B3h', data)
-    print("{}-> x:{} y:{} z:{}".format(device_id, float.fromhex(str(data[3])),
-                                                  float.fromhex(str(data[4])),
-                                                  float.fromhex(str(data[5]))))
+    ax = littleEndianToInt16(data, 3) / 4096
+    ay = littleEndianToInt16(data, 5) / 4096
+    az = littleEndianToInt16(data, 7) / 4096
+    global FIRST_PRINT
+    if FIRST_PRINT:
+        print("Receiving data from {}".format(device_id))
+        FIRST_PRINT = False
+    if abs(ax)>=MOVEMENT_THRESHOLD or abs(ay)>=MOVEMENT_THRESHOLD:
+        print("Device {} is moving".format(device_id))
+    #print("{}-> x:{} y:{} z:{}".format(device_id, ax, ay, az))
 
+
+MOVEMENT_THRESHOLD = 1
+FIRST_PRINT = True
 device_id = str(sys.argv[1])
 
 try:
@@ -26,6 +48,8 @@ try:
     fosh.config['sensor_combination'] = 3
     #accelerometer rate to 100Hz
     fosh.config['accelerometer_rate'] = 0x08
+    #accelerometer range to +-8G
+    fosh.config['accelerometer_range'] = 0x08
 
     #if config is not equal to the fosh.config just send it to the device
     if config != fosh.config:
